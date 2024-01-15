e3acdb9ce30b27f7ad60494688394137b3212b72
steven2308 <steven2308@outlook.com>
2024-01-15 14:18:43 -0500

    Minor improvements on sample contract and deploy script.

--- contracts/SimpleEquippable.sol
+++ contracts/SimpleEquippable.sol
@@ -1,19 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.8.21;
 
+pragma solidity ^0.8.21;
 import "@rmrk-team/evm-contracts/contracts/implementations/lazyMintNative/RMRKEquippableLazyMintNative.sol";
 
 contract SimpleEquippable is RMRKEquippableLazyMintNative {
     constructor(
-        string memory name,
-        string memory symbol,
         string memory collectionMetadata,
         string memory baseTokenURI,
         InitData memory data
     )
         RMRKEquippableLazyMintNative(
-            name,
-            symbol,
+            "SimpleEquippable",
+            "SE",
             collectionMetadata,
             baseTokenURI,
             data
