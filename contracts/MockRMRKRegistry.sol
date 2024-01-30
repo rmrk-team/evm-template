0915f4308f0c0190970ceef60de910f340182f25
Steven Pineda <steven2308@outlook.com>
2024-01-30 10:39:59 -0500

    Several improvements (#5)
    - Uses RMRK package v2.4.3.
    - Adds scripts to deploy utils.
    - Adds scripts to catalog catalog.
    - Uses pnpm instead of yarn.
    - Several typescript improvements.

--- contracts/MockRMRKRegistry.sol
+++ contracts/MockRMRKRegistry.sol
@@ -3,10 +3,12 @@
 pragma solidity ^0.8.21;    
 
 // So typechain gets them:
-import "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKEquipRenderUtils.sol";
-import "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKCatalogUtils.sol";
-import "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKCollectionUtils.sol";
-import "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKBulkWriter.sol";
+import {RMRKEquipRenderUtils} from "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKEquipRenderUtils.sol";
+import {RMRKCatalogUtils} from "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKCatalogUtils.sol";
+import {RMRKCollectionUtils} from "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKCollectionUtils.sol";
+import {RMRKBulkWriter} from "@rmrk-team/evm-contracts/contracts/RMRK/utils/RMRKBulkWriter.sol";
+import {RMRKCatalogImpl} from "@rmrk-team/evm-contracts/contracts/implementations/RMRKCatalogImpl.sol";
+
 
 // Mock RMRK Registry to expose addExternalCollection function.
 contract MockRMRKRegistry {
---
+++ scripts/run-deploy-utils.ts
@@ -0,0 +1,83 @@
+import { ethers, run, network } from 'hardhat';
+import { delay } from './utils';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log('Deployer address: ' + (await deployer.getAddress()));
+
+  const bulkwriterFactory = await ethers.getContractFactory('RMRKBulkWriter');
+  const bulkwriter = await bulkwriterFactory.deploy();
+  await bulkwriter.waitForDeployment();
+  const bulkwriterAddress = await bulkwriter.getAddress();
+  console.log('Bulk Writer deployed to:', bulkwriterAddress);
+
+  const catalogUtilsFactory = await ethers.getContractFactory('RMRKCatalogUtils');
+  const catalogUtils = await catalogUtilsFactory.deploy();
+  await catalogUtils.waitForDeployment();
+  const catalogUtilsAddress = await catalogUtils.getAddress();
+  console.log('Catalog Utils deployed to:', catalogUtilsAddress);
+
+  const collectionUtilsFactory = await ethers.getContractFactory('RMRKCollectionUtils');
+  const collectionUtils = await collectionUtilsFactory.deploy();
+  await collectionUtils.waitForDeployment();
+  const collectionUtilsAddress = await collectionUtils.getAddress();
+  console.log('Collection Utils deployed to:', collectionUtilsAddress);
+
+  const renderUtilsFactory = await ethers.getContractFactory('RMRKEquipRenderUtils');
+  const renderUtils = await renderUtilsFactory.deploy();
+  await renderUtils.waitForDeployment();
+  const renderUtilsAddress = await renderUtils.getAddress();
+  console.log('Equip Render Utils deployed to:', renderUtilsAddress);
+
+  if (network.config.chainId) {
+    // Hardhat
+    return;
+  }
+
+  // sleep 10s
+  delay(10000);
+
+  console.log('Etherscan contract verification starting now.');
+  try {
+    await run('verify:verify', {
+      address: bulkwriterAddress,
+      constructorArguments: [],
+    });
+  } catch (error) {
+    // probably already verified
+  }
+
+  try {
+    await run('verify:verify', {
+      address: catalogUtilsAddress,
+      constructorArguments: [],
+    });
+  } catch (error) {
+    // probably already verified
+  }
+
+  try {
+    await run('verify:verify', {
+      address: collectionUtilsAddress,
+      constructorArguments: [],
+    });
+  } catch (error) {
+    // probably already verified
+  }
+
+  try {
+    await run('verify:verify', {
+      address: renderUtilsAddress,
+      constructorArguments: [],
+    });
+  } catch (error) {
+    // probably already verified
+  }
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
